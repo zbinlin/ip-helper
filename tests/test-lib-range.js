dc88955e9e3be5cc0838b58ef4960d5d258a8371
Zheng Chaoping <zbinlin@gmail.com>
2016-11-07 16:52:43 +0800

    {FEAT}: add other hyphen style range
    like: `127.0.0.0-127.0.0.255`

--- tests/test-lib-range.js
+++ tests/test-lib-range.js
@@ -8,8 +8,11 @@ describe("test isIPRange function", () => {
     it("returns true if arguments 0 is a CIDR-style ip range", () => {
         assert.ok(isIPRange("127.0.0.1/32"));
     });
-    it("returns true if arguments 0 is a hyphen-style ip range", () => {
+    it("returns true if arguments 0 is a hyphen-part-style ip range", () => {
         assert.ok(isIPRange("127.0.0.0-255"));
+    });
+    it("returns true if arguments 0 is a hyphen-full-style ip range", () => {
+        assert.ok(isIPRange("127.0.0.1-127.0.0.255"));
     });
     it("returns false if arguments 0 is not a valid ip range", () => {
         assert.equal(isIPRange("127.0"), false);
@@ -24,8 +27,18 @@ describe("test convertIPRangeToIterator", () => {
         assert.deepEqual([...convertIPRangeToIterator("127.0.1-2.1")], [
             "127.0.1.1", "127.0.2.1",
         ]);
+        assert.deepEqual([...convertIPRangeToIterator("127.0.1.1-127.0.1.1")], [
+            "127.0.1.1",
+        ]);
+        assert.deepEqual([...convertIPRangeToIterator("127.0.1.1-127.0.1.2")], [
+            "127.0.1.1", "127.0.1.2",
+        ]);
+        assert.deepEqual([...convertIPRangeToIterator("127.0.1.2-127.0.1.1")], [
+            "127.0.1.1", "127.0.1.2",
+        ]);
         assert.equal([...convertIPRangeToIterator("127.0.0.1/24")].length, 256);
         assert.equal([...convertIPRangeToIterator("127.0.0.-")].length, 256);
+        assert.equal([...convertIPRangeToIterator("127.0.0.0-127.0.0.255")].length, 256);
     });
     it("throws an error if the arguments is not valid ip range", () => {
         assert.throws(() => {
